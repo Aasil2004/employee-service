7eb80f2ee07889f4f8cd269ad4499b0f7bfa083d
Francis-Aasil <faasilg23@gmail.com>
2026-01-13 07:47:16 +0530

    Implemented Role management system and Navbar

--- payroll/src/main/java/com/example/payroll/LoadDatabase.java
+++ payroll/src/main/java/com/example/payroll/LoadDatabase.java
@@ -21,13 +21,14 @@ class LoadDatabase {
             
             Role developerRole = roleRepository.save(new Role("developer")); 
             Role testerRole = roleRepository.save(new Role("tester"));
+            Role adminRole = roleRepository.save(new Role("admin"));
             roleRepository.save(new Role("manager"));
-            roleRepository.save(new Role("admin"));
             
             log.info("Preloaded Roles.");
 
             log.info("Preloading " + employeeRepository.save(new Employee("Bilbo Baggins", "bilbo", "password123", developerRole)));
             log.info("Preloading " + employeeRepository.save(new Employee("Frodo Baggins", "frodo", "password123", testerRole)));
+            log.info("Preloading " + employeeRepository.save(new Employee("Admin User", "admin", "admin123", adminRole)));
         };
     }
 }
